277fd2799bb8935200609bcae8263b582d2885f3
Nnavarr <noe.navarro1019@gmail.com>
2019-01-08 14:58:22 -0700

    Python Script Update; Importing various data sources to mimic existing list format.

--- Entity List Compilation.py
+++ Entity List Compilation.py
@@ -2,36 +2,22 @@ import pandas as pd
 import pyodbc
 import numpy as np
 
-from getpass import getpass, getuser
-from wand.image import Image as Img
-
-
-# SQL DB Connection ----
-# UID = getuser()
-# PWD = getpass(prompt= 'Type password')
-
-# %s for position of UID and PWD Request  %(UID,PWD)) @ End after the string
-
 # SQL Database Engines ----
-mentity_engine = pyodbc.connect('DRIVER={SQL SERVER}; SERVER=OPSREPORT02.UHAUL.AMERCO.ORG;DATABASE=MEntity;UID'
-                                '=1217543;PWD=Noe543N')
-finanalysis_engine = pyodbc.connect('DRIVER={SQL SERVER}; SERVER=OPSREPORT02.UHAUL.AMERCO.ORG;DATABASE=FINANALYSIS'
-                                    ';UID=1217543;PWD=Noe543N')
-
+mentity_engine = pyodbc.connect('DRIVER={SQL SERVER}; SERVER=OPSREPORT02.UHAUL.AMERCO.ORG;DATABASE=MEntity;UID=1217543;PWD=Noe543N')
+finanalysis_engine = pyodbc.connect('DRIVER={SQL SERVER}; SERVER=OPSREPORT02.UHAUL.AMERCO.ORG;DATABASE=FINANALYSIS;UID=1217543;PWD=Noe543N')
+finaccounting_engine = pyodbc.connect('DRIVER={SQL SERVER}; SERVER=OPSREPORT02.UHAUL.AMERCO.ORG;DATABASE=FinAccounting;UID=1217543;PWD=Noe543N')
 
 # STEP 1: Import Previous ----
 # Import Existing Quarterly Acquisitions List ----
-existing_list = pd.read_csv("Z://group/MIA/Noe/Projects/Same Store & Cap Ex/Report/Quarterly Acquisitions/Master List.csv")
+existing_list = pd.read_csv("Z://group/MIA/Noe/Projects/Post Acquisition/Report/Quarterly Acquisitions/Master List.csv")
 existing_mentity = existing_list["MEntity"]  # Extract MEntity column
 existing_mentity.nunique()
 # 311 Unique MEntity Numbers
-
 
 # STEP 2: Filter DLR01 With Existing MEntity numbers ----
 dlr01_query_initial = "SELECT * FROM ENTITY_DLR01 WHERE MEntity in {} AND [STATUS] = 'O' ORDER BY [ID] ASC, [MEntity]".format(tuple(existing_mentity))
 dlr01_initial = pd.read_sql_query(dlr01_query_initial, mentity_engine)
 dlr01_unique_mentity = dlr01_initial.MEntity.unique()
-
 
 # STEP 3: Determine if the center is still open ----
 mentity_check = np.average((np.isin(existing_mentity, dlr01_unique_mentity)))
@@ -45,17 +31,99 @@ if mentity_check < 1:
 else:
   print("All Existing Acquisitions are accounted for, proceed to new quarter group")
 
-
 # Maintain Master Acquisitions File ----
 writer = pd.ExcelWriter(
-    'Z:/group/MIA/Noe/Projects/Same Store & Cap Ex/Report/Quarterly Acquisitions/Acquisition List Summary.xlsx',
+    'Z:/group/MIA/Noe/Projects/Post Acquisition/Report/Quarterly Acquisitions/Acquisition List Summary.xlsx',
     engine='xlsxwriter')
 
-existing_list.to_excel(writer, sheet_name = 'Included Acquisitions')
+existing_list.to_excel(writer, sheet_name='Included Acquisitions')
 writer.save()
 
 
+
+# Step 2: Import Newly Closed Acquisitions for the Quarter ----
 # Import New Acquisitions List ----
+closed_acquisitions_f19 = pd.read_excel('//adfs01.uhi.amerco/departments/mia/group/MIA/Noe/Projects/Post Acquisition/Report/Quarterly Acquisitions/Closed Acquisitions FY 19.xlsx',sheet_name='Closed Acquisitions FY 19')
+
+# Process imported data frame to match "existing_list" format ----
+# Determine centers from the new quarter that are not present within the current acquisitions list.
+closed_acquisitions_f19 = closed_acquisitions_f19.sort_values(by=['Close of Escrow'])
+new_additions = closed_acquisitions_f19[(closed_acquisitions_f19['Close of Escrow'] >= '2018-10-1')]
+
+# Check for Unique Values ----
+new_additions.Entity.nunique()  # 28 Unique Entity values
+
+# Create Missing Columns ----
+new_additions.insert(0, "Group", 16)  # Most Recent Group ----
+new_additions.insert(1, "FY", 2019)
+new_additions.insert(2, "Quarter", 3)
+
+# Query to import Entity data for new additions ----
+dlr01_query_new = "SELECT * FROM ENTITY_DLR01 WHERE ENTITY_6NO in {} AND [STATUS] = 'O' ORDER BY [ENTITY_6NO] ASC".format(tuple(new_additions.Entity))
+dlr01_new_acquisitions = pd.read_sql_query(dlr01_query_new, mentity_engine)
+dlr01_new_acquisitions.rename(columns={'ENTITY_6NO': 'Entity'}, inplace=True)
+
+# Comparison of lists: EDA ----
+print(new_additions.Entity.value_counts(dropna=False))
+print(new_additions.info(verbose=True))
+
+dlr01_new_acquisitions.Entity.isin(new_additions.Entity.astype(str))
+
+# Duplicate Entity numbers: Removal of Duplicates ----
+duplicate_entity = new_additions[new_additions.Entity == 760077]  # Discovered duplicate through .value_counts
+new_additions_unique = new_additions.drop_duplicates(subset='Entity', keep='last')  # Removes duplicates from df
+
+# Reformat new_additions_unique data frame to match existing list format ----
+cols_existing = list(existing_list.columns.values)
+cols_new = list(new_additions.columns.values)
+print(cols_existing)  # Existing column structure
+print(cols_new)  # New column structure
+
+# Rename new additions columns to match existing ----
+dlr01_new_acquisitions.info(verbose=True)
+
+# Column Search Code ----
+column_search = dlr01_new_acquisitions.loc[:, dlr01_new_acquisitions.columns.str.contains("OWNER")]
+column_search.info(verbose=True)
+
+# Left Join required DLR01 columns on New Acquisitions list ----
+new_additions_unique.Entity = new_additions_unique.Entity.astype(str)
+new_additions_unique = new_additions_unique.merge(dlr01_new_acquisitions.loc[:, ["Entity", "ENTITY_NAME", "MEntity", "ENTITY_TYPE", "DATE_OPENED", "DATE_CLOSED", "STATUS"]], how='left', on='Entity')
+
+# Re-format new additions column to match existing list ----
+new_additions_unique = new_additions_unique[["Group", "FY", "Quarter", "Close of Escrow", "ENTITY_NAME", "MEntity", "Entity", "ENTITY_TYPE", "DATE_OPENED", "DATE_CLOSED", "STATUS", "Address", "City", "State", "Purchase Price", "Property Description"]]
+new_additions_unique.MEntity.nunique()
+
+
+# Step 3: Import Profit Center, Owner, and Construction_Type ----
+sap_hierarchy_query = "SELECT * FROM SAP_Cost_Center_Hierarchy WHERE MEntity in {} ORDER BY [ID] ASC, [MEntity]".format(tuple(new_additions_unique.MEntity))
+sap_hierarchy = pd.read_sql_query(sap_hierarchy_query, finaccounting_engine)
+sap_hierarchy.MEntity.nunique()  # 25 unique MEntity numbers / 28 total new additions
+missing_MEntity = -new_additions_unique.MEntity.astype(str).isin(sap_hierarchy.MEntity)  # Filter missing MEntity
+missing_MEntity_df = new_additions_unique[missing_MEntity]  # DF of missing MEntity observations
+
+sap_hierarchy.MEntity.value_counts()  # Search for any duplicate values within the MEntity column
+# M0000121074 ; 3 observations
+# M0000021212 ; 2 observations
+
+new_additions_unique.info(verbose=True)  # MEntity = Object
+sap_hierarchy.info(verbose=True)  # MEntity = Object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
