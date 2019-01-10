a6dbdc4c12bbe104c60cac593a46172de0ecb5f6
Nnavarr <noe.navarro1019@gmail.com>
2019-01-10 10:40:20 -0700

    Nearly finished with Entity List Aggregation of F19 Q3 ; Commit to save progress.

--- Entity List Compilation.py
+++ Entity List Compilation.py
@@ -32,8 +32,9 @@ else:
   print("All Existing Acquisitions are accounted for, proceed to new quarter group")
 
 
-
-# Step 2: Import Newly Closed Acquisitions for the Quarter ----
+#  -------------------------------------------------------------
+#  Step 2: Import Newly Closed Acquisitions for the Quarter ----
+#  -------------------------------------------------------------
 # Import New Acquisitions List ----
 closed_acquisitions_f19 = pd.read_excel('//adfs01.uhi.amerco/departments/mia/group/MIA/Noe/Projects/Post Acquisition/Report/Quarterly Acquisitions/Closed Acquisitions FY 19.xlsx',sheet_name='Closed Acquisitions FY 19')
 
@@ -88,7 +89,10 @@ new_additions_unique = new_additions_unique[["Group", "FY", "Quarter", "Close of
 new_additions_unique.MEntity.nunique()
 
 
-# Step 3: Import Profit Center, Owner, and Construction_Type ----
+#  ---------------------------------------------------------------
+#  Step 3: Import Profit Center, Owner, and Construction_Type ----
+#  ---------------------------------------------------------------
+
 sap_hierarchy_query = "SELECT * FROM SAP_Cost_Center_Hierarchy WHERE MEntity in {} ORDER BY [MEntity]".format(tuple(new_additions_unique.MEntity))
 sap_hierarchy = pd.read_sql_query(sap_hierarchy_query, finaccounting_engine)
 finaccounting_engine.close()  # Close SQL Engine
@@ -105,7 +109,6 @@ sap_hierarchy_unique = sap_hierarchy.drop_duplicates(subset='MEntity', keep='las
 # Left Join Profit Center on New Additions ----
 new_additions_unique = new_additions_unique.merge(sap_hierarchy_unique.loc[:, ["MEntity", "Cost Center", "Hierarchy Area"]], how='left', on='MEntity')
 
-
 # Import REA Val DB for Construction Type ----
 rea_val_engine = pyodbc.connect('DRIVER={SQL SERVER}; SERVER=OPSREPORT02.UHAUL.AMERCO.ORG;DATABASE=RealEstateValuation;UID=1217543;PWD=Noe543N')
 
@@ -117,32 +120,65 @@ rea_val_engine.close()  # Close the SQL Connection
 new_additions_unique = new_additions_unique.merge(rea_val_db.loc[:, ["MEntity", "Parent_MEntity", "Property_Type", "Construction_Type"]], how='left', on='MEntity')
 
 # Re-arrange oolumns to match "Existing" list ----
-new_additions_unique = new_additions_unique.iloc[:, [0, 1, 2, 3, 4, 5, 6, 16, 7, 8, 9, 10, 11, 12, 13, 14, 17, 20, 19]]
-new_additions_unique.rename(columns={'ENTITY_NAME': 'Entity_Name', 'Cost Center': 'Profit_Center',
-                                     'DATE_OPENED': 'Date_Opened', 'DATE_CLOSED': 'Date_Closed',
-                                     'STATUS': 'Status', 'Purchase Price': 'Purchase',
-                                     'Hierarchy Area': 'Owner'}, inplace=True)
+new_additions_unique = new_additions_unique.iloc[:, [0, 1, 2, 3, 4, 5, 6, 16, 7, 8, 9, 10, 11, 12, 13, 14, 17, 20, 19, 15]]
+new_additions_unique.rename(columns={'ENTITY_NAME':'Entity_Name', 'Cost Center':'Profit_Center', 'ENTITY_TYPE':'Entity_Type', 'DATE_OPENED':'Date_Opened', 'DATE_CLOSED':'Date_Closed', 'STATUS':'Status', 'Purchase Price':'Purchase', 'Hierarchy Area':'Owner'}, inplace=True)
 
 # Remove Missing Profit Center Centers ----
 missing_profit_center_df = new_additions_unique[pd.isnull(new_additions_unique.Profit_Center)]
 new_additions_unique = new_additions_unique[-pd.isnull(new_additions_unique.Profit_Center)]
 
+# Explore the "Owner" column ----
+new_additions_unique.Owner = new_additions_unique.Owner.str[3:7]
+not_UHI = new_additions_unique[new_additions_unique.Owner != 'UHI']  # Property.com does not have an owner listed for the property
 
-# Determine Property Owner ----
-
-
-
-
-
-
+# Remove Non-UHI from the list and continue aggregation ----
+new_additions_unique = new_additions_unique[-new_additions_unique.MEntity.isin(not_UHI.MEntity)]
 
 
-# Maintain Master Acquisitions File ----
+#  -------------------------------
+#  Finalize Acquisitions List ----
+#  -------------------------------
+
+# Filter for Remote / Abutting Properties (use description) ----
+remote_properties_test = new_additions_unique[new_additions_unique['Property Description'].str.contains('.mote')]
+
+# Centers needing to be removed (True Remotes)
+# M0000140057
+# M0000140990
+# M0000139691
+
+removed_remotes = new_additions_unique[new_additions_unique.MEntity.isin(['M0000140057', 'M0000140990', 'M0000139691'])]
+
+# Filter Abutting Properties ----
+abutting_properties_test = new_additions_unique[new_additions_unique['Property Description'].str.contains('.butting')]
+
+# Centers needing to be removed (True Abutting)
+# M0000000400
+# M0000003759
+# M0000001042
+# M0000000906
+# M0000021212
+# M0000121074
+
+# All of the properties above have been identified as abutting and will be removed from the analysis.
+removed_abutting = new_additions_unique[new_additions_unique.MEntity.isin(['M0000000400', 'M0000003759', 'M0000001042', 'M0000000906', 'M0000021212', 'M0000121074'])]
+
+# Finalize New Quarter List ----
+# Remove all remotes / abutting from the df above
+new_additions_unique = new_additions_unique[-new_additions_unique.MEntity.isin(removed_remotes.MEntity)]
+new_additions_unique = new_additions_unique[-new_additions_unique.MEntity.isin(removed_abutting.MEntity)]
+
+#  --------------------------------------
+#  Maintain Master Acquisitions File ----
+#  --------------------------------------
 writer = pd.ExcelWriter(
     'Z:/group/MIA/Noe/Projects/Post Acquisition/Report/Quarterly Acquisitions/Acquisition List Summary.xlsx',
     engine='xlsxwriter')
 
 existing_list.to_excel(writer, sheet_name='Included Acquisitions')
 missing_profit_center_df.to_excel(writer, sheet_name='Missing_PC')
+removed_remotes.to_excel(writer, sheet_name='Removed_Remotes')
+removed_abutting.to_excel(writer, sheet_name='Removed_Abutting')
+not_UHI.to_excel(writer, sheet_name='Not_UHI')
 writer.save()
 
