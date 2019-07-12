0dc34a54fc5f926bbdaf7761a143f5be9e2b441b
Noe Navarro <noe.navarro1019@gmail.com>
2019-07-12 13:23:40 -0700

    Single Profit Center Income Statement Function is now functional. This will need to be passed on a dictionary of individual profit centers.

--- Dashboard Data Compilation.py
+++ Dashboard Data Compilation.py
@@ -260,3 +260,153 @@ income_statement_df.reset_index(inplace=True)
 # Tidy version of income statement ----
 income_statement_df = income_statement_df.melt(id_vars=['Date'])
 
+
+# Checkpoint: Single Center Income Statement Function is functional -
+
+# -------------------------
+# Income Statement Function
+# -------------------------
+
+def income_statement(profit_center, sap_data, line_item_dict):
+
+    """
+    The function will compile an income statement based on a list of profit center numbers
+
+    :param profit_center: Profit Center Dictionary
+    :param sap_data: SAP SQL Connection (imported to Python)
+    :param line_item_dict: Chart of Accounts Dictionary
+    :return:
+    """
+
+    # Step 1: Filter SAP DB  ----
+    pc_mask = sap_data['Profit_Center'] == profit_center
+    temp_sap = sap_data[pc_mask]
+
+    # Chart of accounts into list ----
+    coa_acct_list = [n.Account.astype(str) for n in line_item_dict.values()]
+
+    # Individual Line Item DF Compilation ----
+    line_item_db = []
+    for i, name in zip(coa_acct_list, line_items):
+
+        # Filter SAP Data for individual line item DF ----
+        temp_data = temp_sap[temp_sap['Account'].isin(i)]
+
+        # Aggregate by Date and Account ----
+        temp_data = temp_data.groupby(['Date'])['value'].agg('sum')
+        temp_data = pd.DataFrame(temp_data)
+        temp_data.reset_index(inplace=True)
+
+        # Create a line item name column ----
+        temp_data['lineitem'] = name
+
+        # Append to Container ----
+        line_item_db.append(temp_data)
+
+        continue
+
+    # Flatten list of individual line items ----
+    line_item_df = pd.concat(line_item_db)
+    line_item_df.reset_index(inplace=True, drop=True)
+
+    # SAP Sign Flip due to presentation format ----
+    line_item_df['value'] = np.where(line_item_df['lineitem'].isin(line_items[0:9]),
+                                     line_item_df['value'] * -1,
+                                     line_item_df['value'])
+
+    # Process Income Statement & Calculate NOI ----
+    line_item_melt = line_item_df
+
+    line_item_pivot = line_item_melt.pivot(index='Date', columns='lineitem', values='value').fillna(0)
+
+    # Missing Line Items ----
+    missing_mask = np.isin(line_items, line_item_pivot.columns.values)
+    missing_line = line_items[~missing_mask]
+
+    # Create Place Holder Values for missing line items ----
+    for i in missing_line:
+        line_item_pivot[i] = 0.00
+        continue
+
+    # Calculate U-Box Income ----
+    line_item_pivot['U-BOX INCOME'] = line_item_pivot['U-BOX STORAGE INCOME'] + \
+                                 line_item_pivot['U-BOX OTHER INCOME'] + \
+                                 line_item_pivot['U-BOX DELIVERY INCOME']
+
+    line_item_pivot['NET SALES'] = line_item_pivot['SALES'] - line_item_pivot['COST OF SALES']
+
+    # TODO: Remove space from 'THIRD PARTY LEASE '
+    line_item_pivot['TOTAL REVENUE'] = line_item_pivot['STORAGE INCOME'] + \
+                                  line_item_pivot['NET SALES'] + \
+                                  line_item_pivot['MISCELLANEOUS INCOME'] + \
+                                  line_item_pivot['U-BOX INCOME'] + \
+                                  line_item_pivot['U-MOVE NET COMMISSION'] + \
+                                  line_item_pivot['INTERCOMPANY RENT'] + \
+                                  line_item_pivot['THIRD PARTY LEASE ']
+
+    line_item_pivot['TOTAL OPERATING EXPENSE'] = line_item_pivot['PERSONNEL'] + \
+                                            line_item_pivot['REPAIRS AND MAINTENANCE/GENERAL'] + \
+                                            line_item_pivot['UTILITIES'] + \
+                                            line_item_pivot['TELEPHONE'] + \
+                                            line_item_pivot['ADVERTISING'] + \
+                                            line_item_pivot['SUPPLIES'] + \
+                                            line_item_pivot['RENT-EQUIPMENT/LAND AND BLDGS'] + \
+                                            line_item_pivot['LIABILITY INSURANCE'] + \
+                                            line_item_pivot['PROPERTY TAX'] + \
+                                            line_item_pivot['BAD DEBT EXPENSE'] + \
+                                            line_item_pivot['OTHER OPERATING EXPENSE']
+
+    line_item_pivot['NET OPERATING INCOME'] = line_item_pivot['TOTAL REVENUE'] - line_item_pivot['TOTAL OPERATING EXPENSE']
+
+    # Income Statement Order ----
+    income_statement_order = ['STORAGE INCOME',
+                              'NET SALES',
+                              'MISCELLANEOUS INCOME',
+                              'U-BOX INCOME',
+                              'U-MOVE NET COMMISSION',
+                              'INTERCOMPANY RENT',
+                              'THIRD PARTY LEASE ',
+                              'TOTAL REVENUE',
+                              'PERSONNEL',
+                              'REPAIRS AND MAINTENANCE/GENERAL',
+                              'UTILITIES',
+                              'TELEPHONE',
+                              'ADVERTISING',
+                              'SUPPLIES',
+                              'RENT-EQUIPMENT/LAND AND BLDGS',
+                              'LIABILITY INSURANCE',
+                              'PROPERTY TAX',
+                              'BAD DEBT EXPENSE',
+                              'OTHER OPERATING EXPENSE',
+                              'TOTAL OPERATING EXPENSE',
+                              'NET OPERATING INCOME']
+
+    income_statement_df = line_item_pivot.loc[:, income_statement_order]
+
+    # Remove NOI = 0 rows ----
+    data_presence_mask = income_statement_df['NET OPERATING INCOME'] != 0
+    income_statement_df = income_statement_df[data_presence_mask]
+    income_statement_df.reset_index(inplace=True)
+
+    # Tidy version of income statement ----
+    income_statement_df = income_statement_df.melt(id_vars=['Date'])
+
+    return(income_statement_df)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Test Environment for Income Statement ----
+
+# Function Parameters ----
+test_pc = '7000010541'
+
+test_function = income_statement(profit_center=test_pc,
+                                 sap_data=sap_db,
+                                 line_item_dict=chart_of_accounts)
+
+# Working Function (7/12/2019) ----
+
+
+
+
+
