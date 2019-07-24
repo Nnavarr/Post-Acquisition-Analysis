2307f50af967239d98ffd46899c933a4851098b7
Noe Navarro <noe.navarro1019@gmail.com>
2019-07-24 14:02:35 -0700

    Finished compilation of F20_Q1 master list. Appended new acquisitions to existing master list. Due to time constraints, the function will need to be written and improved at a later time.

--- Quarterly_Acquisitions_Entity_List.py
+++ Quarterly_Acquisitions_Entity_List.py
@@ -88,4 +88,141 @@ assert included_entity.size == unique_entity.size, 'A new additions entity is no
 
 mentity_engine.close()
 
-# -
+# -----------------
+# Reformat new_list
+# -----------------
+cols_existing = list(master_list.columns.values)
+cols_new = list(new_list.columns.values)
+
+# Relevant DLR01 Column names ----
+dlr01_cols = ["Entity", "ENTITY_NAME", "MEntity", "MCO_NUM", "DISTRICT_NO", "ENTITY_TYPE", "DATE_OPENED", "DATE_CLOSED","STATUS"]
+
+# Left Join required DLR01 columns on new_list ----
+new_list = new_list.merge(dlr01_new_acquisitions.loc[:, dlr01_cols], how='left', on='Entity')
+
+# Re-format new additions column to match existing list ----
+new_list = new_list[["Group", "FY", "Quarter", "Close of Escrow", "ENTITY_NAME", "MEntity", "Entity", "MCO_NUM", "DISTRICT_NO", "ENTITY_TYPE", "DATE_OPENED", "DATE_CLOSED", "STATUS", "Address", "City", "State", "Purchase Price", "Property Description"]]
+
+
+#  --------------------------------------------------
+#  Import Profit Center, Owner, and Construction_Type
+#  --------------------------------------------------
+finaccounting_engine = create_connection(database='FinAccounting')
+
+sap_hierarchy_query = "SELECT * FROM SAP_Cost_Center_Hierarchy WHERE MEntity in {} ORDER BY [MEntity]".format(tuple(new_list.MEntity))
+sap_hierarchy = pd.read_sql_query(sap_hierarchy_query, finaccounting_engine)
+
+sap_hierarchy_unique = sap_hierarchy.drop_duplicates(subset='MEntity', keep='last')
+
+# Left Join Profit Center on New Additions ----
+new_list = new_list.merge(sap_hierarchy_unique.loc[:, ["MEntity", "Cost Center", "Hierarchy Area"]], how='left', on='MEntity')
+
+# ---------------------
+# Real Additions Import
+# ---------------------
+
+rea_val_engine = create_connection(database='RealEstateValuation')
+rea_val_query = "SELECT * FROM REV_REAL_ADDITIONS WHERE MEntity in {} ORDER BY [MEntity]".format(tuple(new_list["MEntity"]))
+rea_val_db = pd.read_sql_query(rea_val_query, rea_val_engine)
+rea_val_engine.close()  # Close the SQL Connection
+
+# Duplicate MEntity in rea_val ----
+# TODO: Create a dynamic way to capture duplicate MEntity numbers from this step and have it be an output
+# Duplicate MEntuty = 'M0000121073'
+
+# Drop Duplicate
+rea_val_db.drop_duplicates(subset='MEntity', keep='last', inplace=True)
+
+# Extract "Construction_Type" from DB ----
+new_list = new_list.merge(rea_val_db.loc[:, ["MEntity", "Parent_MEntity", "Property_Type", "Construction_Type"]], how='left', on='MEntity')
+
+# Create DF of missing profit centers (to be exported separately) ----
+
+# Explore the Hierarchy / Owner column ----
+new_list['Hierarchy Area'] = new_list['Hierarchy Area'].str[3:7]
+
+
+# ------------
+# Graph Import
+# ------------
+
+finanalysis_engine = create_connection(database='FINANALYSIS')
+graph_query = "SELECT * FROM GRAPH_ENTITY_INFO WHERE MEntity in {} ORDER BY [MEntity]".format(tuple(new_list['MEntity']))
+graph_db = pd.read_sql_query(graph_query, finanalysis_engine)
+
+#  Left merge long/lat ----
+new_list = pd.merge(new_list, graph_db.loc[:, ["MEntity", "Latitude", "Longitude", 'CBSA']], how='left', on='MEntity')
+
+# ---------------------------
+# Remote / Abutting Properties
+# ----------------------------
+
+def classify_locs(description):
+    description = description.lower()
+    # Set default values
+    abutting = 'No'
+    remote = 'No'
+    # Compile regex statements for classifiers
+    re_remote = {}
+    re_abutting = re.compile(r'abutting\s{0,2}\d+')
+    re_remote[0] = re.compile(r'remote([^ly])')
+    re_remote[1] = re.compile(r'run\s{0,2}from\s{0,2}\d+')
+    re_remote[2] = re.compile(r'\d+.*run.*remote(ly){0,1}')
+    re_remote[3] = re.compile(r'managed\s{0,2}remote(ly){0,1}')
+    # Change flag to "Yes" if string is found
+    if re_abutting.findall(description):
+        abutting = 'Yes'
+    for item in re_remote.values():
+        if item.findall(description):
+            remote = 'Yes'
+            break
+    return(abutting, remote)
+
+# Create Abutting column ----
+new_list['Abutting'], new_list['Remote'] = zip(*new_list['Property Description'].map(classify_locs))
+
+
+# -------------
+# Finalize List
+# -------------
+
+# Rename New Acquisitions Columns ----
+new_list.rename(columns={'Hierarchy Area': 'Simple Owner',
+                               'Cost Center': 'Profit_Center',
+                               'Construction_Type': 'Type',
+                               'Latitude': 'LOC_LATITUDE',
+                               'Longitude': 'LOC_LONGITUDE',
+                               'Parent_MEntity': 'Parent MEntity'},
+                      inplace=True)
+
+new_list['Include?'] = 0
+new_list['Include?'] = np.where((new_list['Abutting'] == 'No') & (new_list['Remote'] == 'No'), 'Yes', 'No')
+new_list['Include?'] = np.where(new_list['Simple Owner'] != 'UHI', 'No', new_list['Include?'])
+
+new_list = new_list.loc[:, ['Group',
+                                 'Close of Escrow',
+                                 'ENTITY_NAME',
+                                 'DISTRICT_NO',
+                                 'MCO_NUM',
+                                 'Entity',
+                                 'Profit_Center',
+                                 'Address',
+                                 'City',
+                                 'State',
+                                 'LOC_LATITUDE',
+                                 'LOC_LONGITUDE',
+                                 'CBSA',
+                                 'Purchase Price',
+                                 'Property Description',
+                                 'Simple Owner',
+                                 'Include?',
+                                 'Type',
+                                 'MEntity',
+                                 'Parent MEntity',
+                                 'Abutting',
+                                 'Remote']]
+
+# Append New Acquisitions
+final_list = master_list.append(new_list, ignore_index=True, sort=False)
+final_list.to_csv(r'Z:\group\MIA\Noe\Projects\Post Acquisition\Quarterly Acquisitions\Acq List\f20_master_list.csv',
+                  index=False)
