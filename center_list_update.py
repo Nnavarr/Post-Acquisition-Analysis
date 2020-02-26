1dcf845a0b9307f710ca095ff3c13ce331c57c4d
Noe Navarro <noe.navarro1019@gmail.com>
2020-02-25 17:01:22 -0700

    "center_list_update" script is up and running. This will be used to import and append new acquisitons on a monthly basis.

--- center_list_update.py
+++ center_list_update.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pyodbc
 import os
 from getpass import getpass
+import sqlalchemy, urllib
 
 user = '1217543'
 
@@ -90,17 +91,106 @@ def new_acquisitions():
 # import relevant lists ----
 new_list, q_acquisitions = new_acquisitions(), quarter_acq_import()
 
-# column name requirements ----
-acquisiton_columns = q_acquisitions.columns.values
-new_list_columns = new_list.columns.values
-present_cols = np.isin(new_list_columns, acquisiton_columns)
+# data prep from individual sources ----
+def dlr01_prep():
 
-new_list_columns[~present_cols]
+    # initial connection / import ----
+    mentity_engine = create_connection(database='MEntity')
+    dlr01_query = "SELECT * FROM [MEntity].[dbo].[ENTITY_DLR01] WHERE [MEntity] in {}".format(tuple(new_list.MEntity))
+    dlr01_df = pd.read_sql_query(dlr01_query, mentity_engine)
+    dlr01_df = dlr01_df.sort_values(['MEntity', 'DATE_CLOSED']).drop_duplicates('MEntity', keep='last').copy()
+    dlr01_df.DATE_EXPIRATION = dlr01_df.DATE_EXPIRATION.astype(str)
 
-# extract current columns to csv for easier comparison of inclusion ----
-new_list.to_csv(r'Z:\group\MIA\Noe\Projects\Post Acquisition\Quarterly Acquisitions\F20 Q4\new_list.csv',
-                index=False)
+    # error check ----
+    assert dlr01_df.DATE_EXPIRATION.all() == '9999-12-31', print("An center is not importing it's most recent Entity iteration")
+    assert dlr01_df.STATUS.all() == 'O', print("Not all centers are showing a status of 'O' for Open")
+    mentity_engine.close()
 
-q_acquisitions.to_csv(r'Z:\group\MIA\Noe\Projects\Post Acquisition\Quarterly Acquisitions\F20 Q4\acq_list.csv',
-                      index=False)
+    # format dlr01 to match existing DF format ----
+    dlr01_cols = ['MEntity', 'ENTITY_NAME', 'MCO_NUM']
+
+    # merge into new list acquisitions ----
+    merge_df = pd.merge(left=new_list, right=dlr01_df[dlr01_cols], on='MEntity', how='left')
+    drop_cols = ['Name', 'FY Closed', 'Quarter Closed', 'Acres', 'Improved SF', 'Entity_Current', 'Extract_Date']
+    merge_df.drop(drop_cols, axis=1, inplace=True)
+
+    return merge_df
+
+def graph_prep(df):
+
+    # initial connection / import ----
+    mentity_engine = create_connection(database='MEntity')
+    index_match_query = "SELECT * FROM [FINANALYSIS].[dbo].[GRAPH_INDEX_MATCH] " \
+                        "WHERE [MEntity] in {}".format(tuple(df.MEntity))
+    index_match_df = pd.read_sql_query(index_match_query, mentity_engine)
+    index_match_df['Remote'] = index_match_df['Parent MEntity'].apply(lambda x: False if x is None else True)
+    entity_info_query = "SELECT * FROM [FINANALYSIS].[dbo].[GRAPH_ENTITY_INFO] " \
+                        "WHERE [MEntity] in {}".format(tuple(new_list.MEntity))
+    entity_info_df = pd.read_sql_query(entity_info_query, mentity_engine)
+    entity_cols = ['MEntity', 'CBSA', 'Cost Center']
+    mentity_engine.close()
+
+    # merge graph db & calculate include column ----
+    graph_df = pd.merge(index_match_df, entity_info_df[entity_cols], on='MEntity', how='left')
+    merge_df = pd.merge(df, graph_df, on='MEntity', how='left', suffixes=['_newlist', '_graph'])
+    merge_df.Abutting = merge_df.Abutting.apply(lambda x: False if x is None else True)
+    merge_df.Remote = merge_df.Remote.apply(lambda x: False if np.isnan(x) else x)
+    merge_df['Include?'] = np.where(merge_df.Remote + merge_df.Abutting == 0, True, False)
+
+    return merge_df
+
+def real_add_prep(df):
+
+    # initial connection / import ----
+    rea_val_engine = create_connection(database='RealEstateValuation')
+    real_additions_query = "SELECT * FROM [RealEstateValuation].[dbo].[REV_REAL_ADDITIONS] " \
+                           "WHERE [MEntity] in {}".format(tuple(new_list.MEntity))
+    real_add_df = pd.read_sql_query(real_additions_query, rea_val_engine)
+    rea_val_engine.close()
+
+    real_add_cols = ['MEntity', 'Construction_Type']
+    merge_df = pd.merge(left=df, right=real_add_df[real_add_cols], on='MEntity', how='left')
+
+    # select appropriate columns ----
+    existing_cols = ['Group', 'Close of Escrow', 'ENTITY_NAME', 'District_newlist', 'MCO_newlist', 'Entity_newlist',
+                     'Cost Center', 'Address', 'City', 'State_newlist', 'Latitude', 'Longitude', 'CBSA',
+                     'Purchase Price',
+                     'Property Description', 'Simple Owner', 'Include?', 'Construction_Type', 'MEntity',
+                     'Parent MEntity',
+                     'Abutting', 'Remote']
+    merge_df = merge_df[existing_cols]
+    merge_df['Purchase Price'] = merge_df['Purchase Price'].astype(int)
+
+    return merge_df
+
+def final_col_rename(df):
+
+    df.rename(columns={'District_newlist': 'DISTRICT_NO',
+                               'MCO_newlist': 'MCO_NUM',
+                               'Entity_newlist': 'Entity',
+                               'Cost Center': 'Profit_Center',
+                               'State_newlist': 'State',
+                               'Latitude': 'LOC_LATITUDE',
+                               'Longitude': 'LOC_LONGITUDE',
+                               'Construction_Type': 'Type'}, inplace=True)
+    return df
+
+# function application & aggregation ----
+df1 = dlr01_prep()
+df2 = graph_prep(df1)
+df3 = real_add_prep(df2)
+final_list = final_col_rename(df3)
+
+# upload to SQL ----
+base_con = (
+    "Driver={{ODBC DRIVER 17 for SQL Server}};"
+    "Server=OPSReport02.uhaul.amerco.org;"
+    "Database=DEVTEST;"
+    "UID={};"
+    "PWD={};"
+).format(user, os.environ.get("sql_pwd"))
+con = pyodbc.connect(base_con)
+params = urllib.parse.quote_plus(base_con)
+engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+final_list.to_sql('Quarterly_Acquisitions_List', engine, index=False, if_exists='append')
 
